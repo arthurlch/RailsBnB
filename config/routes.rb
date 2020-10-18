f5fb2994f01c0c5505e9b244e931470a1ff101bf
Arthur <arthur@pop-os.localdomain>
2020-10-18 22:01:09 +0900

    properties model

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     # Add routes below this line
     resources :users, only: [:create]
     resources :sessions, only: [:create, :destroy]
+    ressources :properties, only: [:index, :show]
 
     get '/authenticated' => 'sessions#authenticated'
 
