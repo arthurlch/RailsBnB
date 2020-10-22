5a58797d2e1890b23d0ba4671bdfa4e5e9e0d3ff
Arthur <arthur@pop-os.localdomain>
2020-10-22 16:39:50 +0900

    fixed staticpagecontroller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,8 @@ class StaticPagesController < ApplicationController
     render 'home'
   end
 
-  def property 
-    @data = { property_id: params[id:] }.to_json
+  def property
+    @data = { property_id: params[:id] }.to_json
     render 'property'
   end
 
