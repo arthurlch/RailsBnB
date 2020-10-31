151038b091734c717f514bc83260349576cb879d
ArthurDav3 <lachat.arthur74@gmail.com>
2020-10-31 12:45:40 +0900

    contoller upd for booking

--- app/javascript/src/property/bookingWidget.jsx
+++ app/javascript/src/property/bookingWidget.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import 'react-dates/initialize';
 import { DateRangePicker } from 'react-dates';
 import { safeCredentials, handleErrors } from '@utils/fetchHelper';
+
 import 'react-dates/lib/css/_datepicker.css';
 
 class BookingWidget extends React.Component {
@@ -24,9 +25,9 @@ class BookingWidget extends React.Component {
           authenticated: data.authenticated,
         })
       })
+
     this.getPropertyBookings();
   }
-
 
   getPropertyBookings = () => {
     fetch(`/api/properties/${this.props.property_id}/bookings`)
@@ -49,20 +50,20 @@ class BookingWidget extends React.Component {
         body: JSON.stringify({
           booking: {
             property_id: this.props.property_id,
-            start_date: null,
-            end_date: null,
+            start_date: startDate.format('MMM DD YYYY'),
+            end_date: endDate.format('MMM DD YYYY'),
           }
         })
     }))
       .then(handleErrors)
       .then(response => {
-        console.log(response);
+        return this.initiateStripeCheckout(response.booking.id)
       })
       .catch(error => {
         console.log(error);
       })
   }
-  
+
   initiateStripeCheckout = (booking_id) => {
     return fetch(`/api/charges?booking_id=${booking_id}&cancel_url=${window.location.pathname}`, safeCredentials({
       method: 'POST',
@@ -70,7 +71,7 @@ class BookingWidget extends React.Component {
       .then(handleErrors)
       .then(response => {
         const stripe = Stripe(process.env.STRIPE_PUBLISHABLE_KEY);
-  
+
         stripe.redirectToCheckout({
           // Make the id field from the Checkout Session creation API response
           // available to this file, so you can provide it as parameter here
@@ -86,60 +87,59 @@ class BookingWidget extends React.Component {
         console.log(error);
       })
   }
-  
 
   onDatesChange = ({ startDate, endDate }) => this.setState({ startDate, endDate })
 
   onFocusChange = (focusedInput) => this.setState({ focusedInput })
 
-  isDayBlocked = day => this.state.existingBookings.filter(b => 
-    day.isBetween(b.start_date, b.end_date, null, '[)')).length > 0
+  isDayBlocked = day => this.state.existingBookings.filter(b => day.isBetween(b.start_date, b.end_date, null, '[)')).length > 0
 
-    
-  
-    render () {
-      const { authenticated, startDate, endDate, focusedInput } = this.state;
-      if (!authenticated) {
-        return (
-          <div className="border p-4 mb-4">
-            Please <a href={`/login?redirect_url=${window.location.pathname}`}>log in</a> to make a booking.
-          </div>
-        );
-      };
-      const { price_per_night } = this.props;
-      let days;
-      if (startDate && endDate) {
-        days = endDate.diff(startDate, 'days');
-      }
+  render () {
+    const { authenticated, startDate, endDate, focusedInput } = this.state;
+    if (!authenticated) {
       return (
         <div className="border p-4 mb-4">
-          <form onSubmit={this.submitBooking}>
-            <h5>${price_per_night} <small>per night</small></h5>
-            <hr/>
-            <div style={{ marginBottom: focusedInput ? '400px': '2rem' }}>
-              <DateRangePicker
-                startDate={startDate} // momentPropTypes.momentObj or null,
-                startDateId="start_date" // PropTypes.string.isRequired,
-                endDate={endDate} // momentPropTypes.momentObj or null,
-                endDateId="end_date" // PropTypes.string.isRequired,
-                onDatesChange={this.onDatesChange}
-                focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-                onFocusChange={this.onFocusChange} // PropTypes.func.isRequired,
-                isDayBlocked={this.isDayBlocked} // block already booked dates
-                numberOfMonths={1}
-              />
-            </div>
-            {days && (
-              <div className="d-flex justify-content-between">
-                <p>Total</p>
-                <p>${(price_per_night * days).toLocaleString()}</p>
-              </div>
-            )}
-            <button type="submit" className="btn btn-large btn-danger btn-block">Book</button>
-          </form>
+          Please <a href={`/login?redirect_url=${window.location.pathname}`}>log in</a> to make a booking.
         </div>
-      )
+      );
+    };
+
+    const { price_per_night } = this.props;
+
+    let days;
+    if (startDate && endDate) {
+      days = endDate.diff(startDate, 'days');
     }
+
+    return (
+      <div className="border p-4 mb-4">
+        <form onSubmit={this.submitBooking}>
+          <h5>${price_per_night} <small>per night</small></h5>
+          <hr/>
+          <div style={{ marginBottom: focusedInput ? '400px': '2rem' }}>
+            <DateRangePicker
+              startDate={startDate} // momentPropTypes.momentObj or null,
+              startDateId="start_date" // PropTypes.string.isRequired,
+              endDate={endDate} // momentPropTypes.momentObj or null,
+              endDateId="end_date" // PropTypes.string.isRequired,
+              onDatesChange={this.onDatesChange}
+              focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+              onFocusChange={this.onFocusChange} // PropTypes.func.isRequired,
+              isDayBlocked={this.isDayBlocked} // block already booked dates
+              numberOfMonths={1}
+            />
+          </div>
+          {days && (
+            <div className="d-flex justify-content-between">
+              <p>Total</p>
+              <p>${(price_per_night * days).toLocaleString()}</p>
+            </div>
+          )}
+          <button type="submit" className="btn btn-large btn-danger btn-block">Book</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default BookingWidget;
