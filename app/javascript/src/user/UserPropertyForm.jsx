ba83558ef05d38192455bdb1dc2497261c4b4406
ArthurDav3 <lachat.arthur74@gmail.com>
2020-12-03 22:00:14 +0900

    updt for property form

--- app/javascript/src/user/UserPropertyForm.jsx
+++ app/javascript/src/user/UserPropertyForm.jsx
@@ -4,7 +4,6 @@ export default class UserPropertyForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = { 
-    properties: [{
       id: "",
       title: "",
       description: "",
@@ -16,10 +15,9 @@ export default class UserPropertyForm extends React.Component {
       bedrooms: "",
       beds: "",
       baths: ""
-      }]
     }
-    this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(event) {
@@ -28,9 +26,41 @@ export default class UserPropertyForm extends React.Component {
     })
   }
 
-  handleSubmit(event) {
-    event.preventDefault()
-    alert('you submited the form')
+  handleSubmit(e) {
+    if (e) { e.preventDefault(); }
+    this.setState({
+      error: '',
+    });
+
+    fetch(`/api/users/${this.props.user_id}`, safeCredentials({
+      method: 'POST',
+      body: JSON.stringify({
+        property: {
+          id: this.state.id,
+          title: this.state.title,
+          description: this.state.description,
+          city: this.state.city,
+          country: this.state.country,
+          property_type: this.state.property_type,
+          price_per_night: this.state.price_per_night,
+          max_guests: this.state.max_guests,
+          bedrooms: this.state.bedrooms,
+          beds: this.state.beds,
+          baths: this.state.baths
+        }
+      })
+    }))
+      .then(handleErrors)
+      .then(data => {
+        if (data.user) {
+          this.login();
+        }
+      })
+      .catch(error => {
+        this.setState({
+          error: 'Could not sign up.',
+        })
+      })
   }
   
  render () {
@@ -39,63 +69,63 @@ export default class UserPropertyForm extends React.Component {
       <form onSubmit={this.handleSubmit} id="userPropertyForm">
         
         <div className="form-group">
-          <label className="col" htmlFor="form-property-title">Name:</label>
-          <input className="form-control form-control-sm" id="form-property-title" 
-            rows="1" name="title" type="text" value={this.state.title} onChange={this.handleChange}/>
+          <label className="col" htmlFor="title">Name:</label>
+          <input className="form-control form-control-sm" id="title" 
+            rows="1" name="title" value={this.state.title || ''} onChange={this.handleChange}/>
         </div>
 
         <div className="form-group">
           <label className="col" htmlFor="form-property-description">Description:</label>
           <textarea className="form-control form-control-sm" id="form-property-description" 
-            rows="2" name="description" value={this.state.description} onChange={this.handleChange}></textarea>
+            rows="2" name="description" value={this.state.description || ''} onChange={this.handleChange}></textarea>
         </div>
 
         <div className="form-group">
           <label className="col" htmlFor="form-property-city">City:</label>
           <input className="form-control form-control-sm" id="form-property-city"
-             rows="3" name="city" value={this.state.city} onChange={this.handleChange}/>
+             rows="3" name="city" value={this.state.city || ''} onChange={this.handleChange}/>
         </div>
         
         <div className="form-group">
           <label className="col" htmlFor="form-property-country">Country:</label>
           <input className="form-control form-control-sm" id="form-property-country" 
-            rows="1" name="country" value={this.state.country} onChange={this.handleChange} />
+            rows="1" name="country" value={this.state.country || ''} onChange={this.handleChange} />
         </div>
 
         <div className="form-group">
           <label className="col" htmlFor="form-property-type">Property type:</label>
           <input className="form-control form-control-sm" id="form-property-type" 
-            rows="1" name="property_type" value={this.state.property_type} onChange={this.handleChange} />
+            rows="1" name="property_type" value={this.state.property_type || ''} onChange={this.handleChange} />
         </div>
 
         <div className="form-group">
           <label className="col" htmlFor="price_per_night">Price per Night:</label>
           <input className="form-control form-control-sm" id="price-per-night" 
-            rows="1" name="price_per_night" value={this.state.price_per_night} onChange={this.handleChange} />
+            rows="1" name="price_per_night" value={this.state.price_per_night || ''} onChange={this.handleChange} />
         </div>
 
         <div className="form-group">
           <label className="col" htmlFor="max_guests">Max guests:</label>
           <input className="form-control form-control-sm" id="max-guests" 
-            rows="1" name="max_guests" value={this.state.max_guests} onChange={this.handleChange}/>
+            rows="1" name="max_guests" value={this.state.max_guests || ''} onChange={this.handleChange}/>
         </div>
 
         <div className="form-group">
           <label className="col" htmlFor="bedrooms">Bedrooms:</label>
           <input className="form-control form-control-sm" id="bedrooms" 
-            rows="1" name="bedrooms" value={this.state.bedrooms} onChange={this.handleChange}/>
+            rows="1" name="bedrooms" value={this.state.bedrooms || ''} onChange={this.handleChange}/>
         </div>
 
         <div className="form-group">
           <label className="col" htmlFor="beds">Beds:</label>
           <input className="form-control form-control-sm" id="beds" 
-            rows="1" name="beds" value={this.state.beds} onChange={this.handleChange}/>
+            rows="1" name="beds" value={this.state.beds || ''} onChange={this.handleChange}/>
         </div>
 
         <div className="form-group">
           <label className="col" htmlFor="baths">Baths:</label>
           <input className="form-control form-control-sm" id="baths" 
-            rows="1" name="baths" value={this.state.baths} onChange={this.handleChange}/>
+            rows="1" name="baths" value={this.state.baths || ''} onChange={this.handleChange}/>
         </div>
 
         <button className="btn btn-sm btn-danger" type="submit">Submit</button>
