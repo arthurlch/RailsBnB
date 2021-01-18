c7c09bfb6298d19f61214a1e02292dc1e1503863
arthur <arthur@pop-os.localdomain>
2021-01-18 16:55:03 +0900

    [UPDATE] Update route for properties

--- app/javascript/src/user/UserAddPropertyForm.jsx
+++ app/javascript/src/user/UserAddPropertyForm.jsx
@@ -6,7 +6,7 @@ export default class UserPropertyForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = { 
-      id: 131,
+      id: title,
       images: [],
       title: "",
       description: "",
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
     # Add routes below this line
     resources :users, only: [:create, :show, :update] 
     resources :sessions, only: [:create, :destroy]
-    resources :properties, only: [:index, :show, :index, :create]
-    resources :bookings, only: [:create, :show, :index]
+    resources :properties, only: [:index, :show, :index, :create, :update]
+    resources :bookings, only: [:create, :show, :index, :update]
     resources :charges, only: [:create]
     get '/properties/:id/bookings' => 'bookings#get_property_bookings'
     get '/authenticated' => 'sessions#authenticated'
