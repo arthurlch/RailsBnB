6f899c07af387e0746cb31a8c03f2574a4082f7f
arthurdav3 <lachat.arthur74@gmail.com>
2021-01-18 16:59:12 +0900

    checking github push issues

--- app/javascript/src/property/editProperty.jsx
+++ app/javascript/src/property/editProperty.jsx
@@ -1,6 +1,9 @@
 // editProperty.jsx
+// 
 import React from 'react';
 import Layout from '@src/layout';
 import BookingWidget from './bookingWidget';
 import { handleErrors } from '@utils/fetchHelper';
 import './property.scss';
+
+
