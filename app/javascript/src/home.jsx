970214e0b61093ee67816d76cd5c8bc1584c51d7
arthurdav3 <lachat.arthur74@gmail.com>
2021-03-12 10:35:22 +0900

    update

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -53,11 +53,10 @@ class Home extends React.Component {
           <p className="text-secondary mb-3">Explore some of the best-reviewed stays in the world</p>
           <div className="row">
             {properties.map(property => {
-              {console.log(property.images)}
               return (
                 <div key={property.id} className="col-6 col-lg-4 mb-4 property">
                   <a href={`/property/${property.id}`} className="text-body text-decoration-none">
-                    <div className="property-image mb-1 rounded" style={{ backgroundImage: `url(${property.image_url})` }} />
+                    <div className="property-image mb-1 rounded" style={{ backgroundImage: `url(${property.images})` }} />
                     <p className="text-uppercase mb-0 text-secondary"><small><b>{property.city}</b></small></p>
                     <h6 className="mb-0">{property.title}</h6>
                     <p className="mb-0"><small>${property.price_per_night} USD/night</small></p>
--- app/views/api/properties/index.jbuilder
+++ app/views/api/properties/index.jbuilder
@@ -9,8 +9,7 @@ json.properties do
     json.country property.country
     json.property_type property.property_type
     json.price_per_night property.price_per_night
-    json.image_url property.image_url
-    json.images property.images
+    json.images property.image_url
     json.bookings do
       json.array! property.bookings do |booking|
         json.id booking.id
