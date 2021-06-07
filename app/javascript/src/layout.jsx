5d1bb683958546dbca3e9946fde30f121bface7c
arthurlch <lachat.arthur74@gmail.com>
2021-06-07 16:35:29 +0900

    feat: remove wouter, nav styling Why:
    *
    
    This change addresses the need by:
    
    *

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -1,35 +1,45 @@
 // layout.js
 import React from 'react';
 import './layout.scss';
-// import {BrowserRouter as Router, Switch, Route } from 'react'
-// react router, routing set up in layout.js
+import { Router, Route, Link, Switch } from "wouter";
+import signupWidget from './login/signupWidget'
+import Home from './home'
+import Login from './login/index'
+
 const Layout = (props) => {
+  
   return (
     <React.Fragment>
-      <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="home" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Profile</a>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <button className="btn btn-info" type="submit">Become a host</button>
-              <button className="btn btn-outline-info ms-3" type="submit">Login</button>
-            </form>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
+              data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
+              aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="home" >Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link">Profile</a>
+                </li>
+              </ul>
+              <form className="d-flex">
+                <button className="btn btn-info" type="submit">
+                  Login
+                </button>
+                <button className="btn btn-outline-info ms-3" type="submit">
+                  Become a host
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-      </nav>
-      </div>
+        </nav>
+      </header>
       
       {props.children}
 
