b42f0751d82bc1e7ac1d83687a59c9bc0bcd9f4e
arthurlch <lachat.arthur74@gmail.com>
2021-06-10 19:40:54 +0900

    feat : navbar styling

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -1,45 +1,51 @@
 // layout.js
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import './layout.scss';
-import { Router, Route, Link, Switch } from "wouter";
-import signupWidget from './login/signupWidget'
-import Home from './home'
-import Login from './login/index'
 
 const Layout = (props) => {
   
+  const [user, setUser] = useState([])
+    useEffect(() => {
+        fetch("/api/users/").then(
+            res => setUser(res.data)
+        )
+    })
+
   return (
     <React.Fragment>
-      <header>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
-              data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
-              aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="home" >Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link">Profile</a>
-                </li>
-              </ul>
-              <form className="d-flex">
-                <button className="btn btn-info" type="submit">
-                  Login
+      
+        <header>
+          <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                  aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon" />
                 </button>
-                <button className="btn btn-outline-info ms-3" type="submit">
-                  Become a host
-                </button>
-              </form>
-            </div>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                      <a href="/">Home</a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="/user/:id">Profile</a>
+                    </li>
+                  </ul>
+                  <form className="d-flex">
+                    <button className="btn btn-info" type="submit">
+                      <a href="/login">Login</a>
+                    </button>
+                    <button className="btn btn-outline-info ms-3" type="submit">
+                      <a href="/login">Become a host</a>
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </nav>
           </div>
-        </nav>
-      </header>
+        </header>
       
       {props.children}
 
