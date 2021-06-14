64e0da9dfbbb2e1c9cc739a44d94dc396ec36759
arthurlch <lachat.arthur74@gmail.com>
2021-06-14 15:18:24 +0900

    feat: nav styles

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -9,17 +9,17 @@ const Layout = (props) => {
       
         <header>
           <div>
-            <nav className="navbar navbar-expand-lg">
+            <nav className="navbar navbar-expand-lg navbar-light">
               <div className="container-fluid">
                 <a className="navbar-brand" href="/">RailsBnB</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
                   data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
                   aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon" />
+                  <span className="navbar-toggler-icon" id="nav-toggler" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
+                    <li className="nav-item me-2">
                       <a href="/">Home</a>
                     </li>
                     <li className="nav-item">
@@ -27,11 +27,11 @@ const Layout = (props) => {
                     </li>
                   </ul>
                   <form className="d-flex">
-                    <button className="btn btn-info" type="submit">
-                      <a href="/login">Login</a>
+                    <button className="btn btn-info"  type="submit">
+                      <a href="/login" id="login">Login</a>
                     </button>
                     <button className="btn btn-outline-info ms-3" type="submit">
-                      <a href="/login">Become a host</a>
+                      <a href="/login" id="become-host">Become a host</a>
                     </button>
                   </form>
                 </div>
