c4b994b98bd6fec0d26cc1d0e56e2d1d6a051306
arthurlch <lachat.arthur74@gmail.com>
2021-11-02 23:01:30 +0900

    fix: test for homepage issues

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,9 +11,13 @@ module Api
       end
     end
     
-   # skip_before_action :verify_authenticity_token
+   skip_before_action :verify_authenticity_token
 
     def show
+      token = cookies.signed[:airbnb_session_token]
+      session = Session.find_by(token: token)
+      return render json: { error: 'user not logged in' }, status: :unauthorized if !session
+
       @user = User.find_by(id: params[:id])
       return render json: { error: 'not_found' }, status: :not_found if !@user
       render 'api/users/show', status: :ok
--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -5,7 +5,6 @@ import Layout from '@src/layout';
 import { handleErrors } from '@utils/fetchHelper';
 import './home.scss';
 import homeImg from '../src/image/railsbnbtrip.png'
-import { data } from 'jquery';
  
 class Home extends React.Component {
   state = {
