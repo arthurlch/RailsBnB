e60ee85f407b41943e1c786c95428e3d459e3f31
arthurlch <lachat.arthur74@gmail.com>
2021-11-02 12:31:50 +0900

    fix: controller cleanup

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,22 +10,19 @@ module Api
         render json: { success: false }, status: :bad_request
       end
     end
-    
-    # Safety check user show and update route cannot be seen if User is not logged in.
-    #skip_before_action :verify_authenticity_token 
-
+     
     def show
       token = cookies.signed[:airbnb_session_token]
       session = Session.find_by(token: token)
       return render json: { error: 'user not logged in' }, status: :unauthorized if !session
-      
+
       @user = User.find_by(id: params[:id])
       return render json: { error: 'not_found' }, status: :not_found if !@user
       render 'api/users/show', status: :ok
     end
 
     def update
-      
+  
     end
     
     private
