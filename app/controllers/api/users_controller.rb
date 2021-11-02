8bee4e1756dc49cea3c6c2d40aaab254e1d944db
arthurlch <lachat.arthur74@gmail.com>
2021-11-02 12:30:52 +0900

    fix: user need to be logged in before to render json from user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,6 +15,10 @@ module Api
     #skip_before_action :verify_authenticity_token 
 
     def show
+      token = cookies.signed[:airbnb_session_token]
+      session = Session.find_by(token: token)
+      return render json: { error: 'user not logged in' }, status: :unauthorized if !session
+      
       @user = User.find_by(id: params[:id])
       return render json: { error: 'not_found' }, status: :not_found if !@user
       render 'api/users/show', status: :ok
