cb673327bf06cfc1af5a999949dad16653e3bfdb
arthurlch <lachat.arthur74@gmail.com>
2021-11-02 18:14:53 +0900

    fix:

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -6,7 +6,7 @@ import { handleErrors } from '@utils/fetchHelper';
 import './home.scss';
 import homeImg from '../src/image/railsbnbtrip.png'
 import { data } from 'jquery';
-
+ 
 class Home extends React.Component {
   state = {
     properties: [],
